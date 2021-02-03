4d1a1f4cfbe38f5ee51812f35d6163b3b01cfe93
rzephie <75554074+rzephie@users.noreply.github.com>
2021-02-03 18:58:18 +0700

    Add files via upload

---
+++ SudokuGame/src/Sudoku/SudokuApplication.java
@@ -0,0 +1,31 @@
+package Sudoku;
+import Sudoku.buildlogic.SudokuBuildLogic;
+import Sudoku.userinterface.IUserInterfaceContract;
+import Sudoku.userinterface.UserInterfaceImpl;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SudokuApplication extends Application{
+    private  IUserInterfaceContract.View uiImpl;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic. build (uiImpl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ SudokuGame/src/Sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package Sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congrats! You have won the game!";
+    public static final String ERROR = "An error has occurred.";
+}
